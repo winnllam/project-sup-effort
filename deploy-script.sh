84079346e75adae3c60af37def141718be7573ef
Winn <winnieee.lam00@gmail.com>
2023-03-04 17:17:05 -0500

    Fixed deploy script for react

--- deploy-script.sh
+++ deploy-script.sh
@@ -2,6 +2,7 @@ ssh ${user}@${host}
 # insert password here
 
 cd project-sup-effort
+git stash
 git pull
 # may need to log in to github
 
@@ -11,6 +12,9 @@ pm2 startup ubuntu
 
 cd ../frontend
 npm install
+npm run build
+pm2 serve build
+pm2 start ubuntu
 
 cd /var/www/html
 rm -r divideandconquer.me

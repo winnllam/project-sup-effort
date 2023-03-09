8f19e670760534de1983b39cd53772987f5f19cb
Winn <winnieee.lam00@gmail.com>
2023-03-09 13:03:34 -0500

    Retrieved auth0 profile info

--- frontend/src/components/Dashboard-Profile/Dashboard-Profile.js
+++ frontend/src/components/Dashboard-Profile/Dashboard-Profile.js
@@ -1,35 +1,47 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./Dashboard-Profile.module.css";
 import profile from "../../assets/profile.svg";
 import dashboardStyles from "../../pages/Dashboard/Dashboard.module.css";
+import * as userService from "../../services/api/Users.js";
 
 const DashboardProfile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  userService.getAllUsers().then((res) => console.log(res));
+
   return (
-    <div className={styles.profile}>
-      <div class={styles.section}>
-        <div class={styles.subtitle}>User Profile</div>
-        <div class={styles.box}>
-          <b>Name:</b> John Doe <br />
-          <b>Username:</b> johndoe123
-          <br />
-          <b>Email:</b> john.doe@email.com <br />
-          <b>Connection:</b> Email <br />
-          <b>Member Since:</b> March 1, 2023
+    isAuthenticated && (
+      <div className={styles.profile}>
+        <div class={styles.section}>
+          <div class={styles.subtitle}>User Profile</div>
+          <div class={styles.box}>
+            <b>Name:</b> {user.name} <br />
+            <b>Username:</b> johndoe123
+            <br />
+            <b>Email:</b> {user.email} <br />
+            <b>Connection:</b> Email <br />
+            <b>Member Since:</b> March 1, 2023
+          </div>
         </div>
-      </div>
 
-      <div class={styles.section}>
-        <div class={styles.subtitle}>Premium Status</div>
-        <div class={styles.box}>
-          <b>Premium Account:</b> Yes <br />
-          <b>Expiry Date:</b> March 31, 2023
-          <br />
-          <b>Plan Type:</b> Monthly
+        <div class={styles.section}>
+          <div class={styles.subtitle}>Premium Status</div>
+          <div class={styles.box}>
+            <b>Premium Account:</b> Yes <br />
+            <b>Expiry Date:</b> March 31, 2023
+            <br />
+            <b>Plan Type:</b> Monthly
+          </div>
         </div>
-      </div>
 
-      <img src={profile} class={dashboardStyles.graphics} alt="profile"></img>
-    </div>
+        <img src={profile} class={dashboardStyles.graphics} alt="profile"></img>
+      </div>
+    )
   );
 };
 

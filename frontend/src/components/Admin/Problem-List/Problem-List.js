b2a1f4b557e0a138b32097bc0bf957d0a106451f
Tony Cao <tony.cao1@outlook.com>
2023-03-12 18:54:21 -0400

    feat: added problem list for admin users

---
+++ frontend/src/components/Admin/Problem-List/Problem-List.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styles from "./Problem-List.module.css";
+
+class ProblemList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={styles.problems}>
+        <div class={styles.title}>Problems</div>
+        <div class={styles.problem}>
+          <div class={styles.problemName}>
+            <b>Fizzbuzz</b>
+          </div>
+          <div class={styles.problemDesc}>
+            <i> Problem Description</i>
+          </div>
+          <div class={styles.editProblem}>
+            <button class={styles.button}>Edit</button>
+          </div>
+        </div>
+
+        <div class={styles.problem}>
+          <div class={styles.problemName}>
+            <b>Fizzbuzz</b>
+          </div>
+          <div class={styles.problemDesc}>
+            <i> Problem Description</i>
+          </div>
+          <div class={styles.editProblem}>
+            <button class={styles.button}>Edit</button>
+          </div>
+        </div>
+
+        <div class={styles.problem}>
+          <div class={styles.problemName}>
+            <b>Fizzbuzz</b>
+          </div>
+          <div class={styles.problemDesc}>
+            <i> Problem Description</i>
+          </div>
+          <div class={styles.editProblem}>
+            <button class={styles.button}>Edit</button>
+          </div>
+        </div>
+
+        <div class={styles.problem}>
+          <div class={styles.problemName}>
+            <b>Fizzbuzz</b>
+          </div>
+          <div class={styles.problemDesc}>
+            <i> Problem Description</i>
+          </div>
+          <div class={styles.editProblem}>
+            <button class={styles.button}>Edit</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProblemList;

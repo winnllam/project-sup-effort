8597e23ef65759115823a1b73dff9077ff25de86
Tony Cao <tony.cao1@outlook.com>
2023-03-14 20:16:39 -0400

    feat: edit test cases

--- frontend/src/components/Admin/Test-Case/Test-Case.js
+++ frontend/src/components/Admin/Test-Case/Test-Case.js
@@ -1,74 +1,103 @@
 import React from "react";
 import styles from "./Test-Case.module.css";
 import Accordion from "react-bootstrap/Accordion";
-import { useRef } from "react";
 
 class TestCase extends React.Component {
-  //   constructor(props) {
-  //     super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      testList: [],
+    };
 
-  //     const itemsRef = useRef(null);
+    for (let i = 0; i < 10; i++) {
+      this.state.testList.push({
+        id: i,
+        desc: "Desc",
+        input: 5,
+        output: "Fizz",
+        isEditing: false,
+      });
+    }
+  }
+  saveTest = (testId) => (e) => {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
 
-  //     const testList = [];
-  //     for (let i = 0; i < 10; i++) {
-  //       testList.push({
-  //         id: i,
-  //         desc: "Desc",
-  //         input: 5,
-  //         output: "Fizz",
-  //       });
-  //     }
-  //   }
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
 
-  //   getMap() {
-  //     if (!this.itemsRef.current) {
-  //       // Initialize the Map on first usage.
-  //       itemsRef.current = new Map();
-  //     }
-  //     return itemsRef.current;
-  //   }
+    const id = this.state.testList.findIndex(function (test) {
+      return test.id === testId;
+    });
+    const newList = this.state.testList;
+    newList[id].isEditing = false;
+    newList[id].input = formJson.input;
+    newList[id].output = formJson.output;
+    this.setState({ testList: newList });
+    console.log(this.state.testList);
+  };
 
-  //   editTestCase(testId) {
-  //     const map = getMap();
-  //     const node = map.get(itemId);
-  //     node.scrollIntoView({
-  //       behavior: "smooth",
-  //       block: "nearest",
-  //       inline: "center",
-  //     });
-  //   }
-  editTest() {}
+  editTest(testId) {
+    const id = this.state.testList.findIndex(function (test) {
+      return test.id === testId;
+    });
+    const newList = this.state.testList;
+    newList[id].isEditing = true;
+    this.setState({ testList: newList });
+  }
 
   render() {
     return (
       <div className={styles.testCase}>
         <Accordion defaultActiveKey="0" class={styles.oneTest}>
-          <Accordion.Item eventKey="0" id={1}>
-            <Accordion.Header>
-              <div class={styles.testHeader}>Test Case #1</div>
-            </Accordion.Header>
-            <Accordion.Body>
-              <div class={styles.testBody}>
-                <b>Input:</b>{" "}
-                <input
-                  type="text"
-                  value={5}
-                  class={styles.inputBox}
-                  readOnly={true}
-                ></input>{" "}
-                <br />
-                <b>Output:</b>{" "}
-                <input
-                  type="text"
-                  value={"Fizz"}
-                  class={styles.inputBox}
-                  readOnly={true}
-                ></input>{" "}
-                <br />
-              </div>
-              <button class={styles.button}>Edit</button>
-            </Accordion.Body>
-          </Accordion.Item>
+          {this.state.testList.map((test) => (
+            <Accordion.Item key={test.id} eventKey={test.id} id={test.id}>
+              <Accordion.Header>
+                <div class={styles.testHeader}>Test Case #{test.id}</div>
+              </Accordion.Header>
+              <Accordion.Body>
+                <div class={styles.testBody}>
+                  <form onSubmit={this.saveTest(test.id)}>
+                    <b>Input:</b>{" "}
+                    <input
+                      type="text"
+                      defaultValue={test.input}
+                      class={styles.inputBox}
+                      readOnly={!test.isEditing}
+                      onChange={this.handleChange}
+                      name="input"
+                      id="input"
+                    ></input>{" "}
+                    <br />
+                    <b>Output:</b>{" "}
+                    <input
+                      type="text"
+                      defaultValue={test.output}
+                      class={styles.inputBox}
+                      readOnly={!test.isEditing}
+                      name="output"
+                    ></input>{" "}
+                    <br />
+                    {!test.isEditing && (
+                      <button
+                        class={styles.button}
+                        onClick={() => this.editTest(test.id)}
+                      >
+                        Edit
+                      </button>
+                    )}
+                    {test.isEditing && (
+                      <button class={styles.button} type="submit">
+                        Save
+                      </button>
+                    )}
+                  </form>
+                </div>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
         </Accordion>
       </div>
     );

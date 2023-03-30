0b1747b2456cfc746edc0284eb5f78429ae75343
Winn <winnieee.lam00@gmail.com>
2023-03-30 13:16:25 -0400

    Get lobby to get problem

--- backend/routers/lobby_router.js
+++ backend/routers/lobby_router.js
@@ -7,16 +7,27 @@ export const lobbyRouter = Router();
 lobbyRouter.post("/:id", async function (req, res, next) {
   const lobby = new Lobby({
     id: req.params.id,
-    host:  "abc",
+    host: "abc",
     players: ["abc"],
     status: "Waiting",
     problem: 1,
-    });
+  });
 
   try {
     await lobby.save();
   } catch (error) {
     return res.status(422).json({ message: error.message });
   }
+  return res.json({ lobby });
+});
+
+lobbyRouter.get("/:id", async function (req, res, next) {
+  const lobby = await Lobby.findOne({ id: req.params.id });
+  if (!lobby) {
+    return res
+      .status(404)
+      .json({ error: "lobby:" + req.params.id + " does not exist" });
+  }
+
   return res.json({ lobby });
 });
--- frontend/src/services/api/Lobby.js
+++ frontend/src/services/api/Lobby.js
@@ -5,3 +5,7 @@ const URL = "/lobby";
 export const createLobby = function (lobbyId) {
   return post(URL + `/${lobbyId}`).then((res) => res.data);
 };
+
+export const getLobby = function (lobbyId) {
+  return get(URL + `/${lobbyId}`).then((res) => res.data.lobby);
+};

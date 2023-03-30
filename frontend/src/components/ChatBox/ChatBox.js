a6104986035eed95ca262e08cf0887a132db9b04
Winn <winnieee.lam00@gmail.com>
2023-03-30 11:54:52 -0400

    Fixed localhost url

--- frontend/src/components/ChatBox/ChatBox.js
+++ frontend/src/components/ChatBox/ChatBox.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import chatboxStyles from './ChatBox.module.css';
+import React from "react";
+import chatboxStyles from "./ChatBox.module.css";
 import { io } from "socket.io-client";
 
 class ChatBox extends React.Component {
   constructor(props) {
     super(props);
-    this.socket = io("http://localhost:9000");
+    let url = process.env.REACT_APP_BACKEND_LOCALHOST;
+    if (process.env.NODE_ENV === "production") {
+      url = process.env.REACT_APP_PRODUCTION_URL;
+    }
+    this.socket = io(url);
     this.state = {
-      messages: []
+      messages: [],
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
     this.socket.on("connect", () => {
-        console.log(`Connected to socket server with id ${this.socket.id}`);
-        this.socket.emit("user-connected", this.socket.id);
-        let newMessage = this.socket.id + " has joined the chat";
-        this.setState({
-          messages: [...this.state.messages, newMessage]
-        });
+      console.log(`Connected to socket server with id ${this.socket.id}`);
+      this.socket.emit("user-connected", this.socket.id);
+      let newMessage = this.socket.id + " has joined the chat";
+      this.setState({
+        messages: [...this.state.messages, newMessage],
+      });
     });
     this.socket.on("user-connected", (message) => {
-        let newMessage = message + " has joined the chat";
-        this.setState({
-          messages: [...this.state.messages, newMessage]
-        });
+      let newMessage = message + " has joined the chat";
+      this.setState({
+        messages: [...this.state.messages, newMessage],
+      });
     });
     this.socket.on("receive-message", (message) => {
-        this.setState({
-          messages: [...this.state.messages, message]
-        });
+      this.setState({
+        messages: [...this.state.messages, message],
+      });
     });
   }
 
@@ -40,13 +44,13 @@ class ChatBox extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const messageInput = document.getElementById('message-input');
+    const messageInput = document.getElementById("message-input");
     const newMessage = this.socket.id + ": " + messageInput.value;
     this.setState({
-      messages: [...this.state.messages, newMessage]
+      messages: [...this.state.messages, newMessage],
     });
     this.socket.emit("send-message", newMessage);
-    messageInput.value = '';
+    messageInput.value = "";
   }
 
   render() {
@@ -56,17 +60,26 @@ class ChatBox extends React.Component {
       <div className={chatboxStyles.chatbox}>
         <ul className={chatboxStyles.messagesList}>
           {messages.map((message, index) => (
-            <li key={index} className={chatboxStyles.mesagesListLi}>{message}</li>
+            <li key={index} className={chatboxStyles.mesagesListLi}>
+              {message}
+            </li>
           ))}
         </ul>
         <div className={chatboxStyles.messageForm}>
           <form id="message-form" onSubmit={this.handleSubmit}>
-            <input 
-              type="text" 
-              className={chatboxStyles.messageInput} 
+            <input
+              type="text"
+              className={chatboxStyles.messageInput}
               id="message-input"
-              placeholder="Type a message..." />
-            <button type="submit" className={chatboxStyles.sendButton} id="send-button">Send</button>
+              placeholder="Type a message..."
+            />
+            <button
+              type="submit"
+              className={chatboxStyles.sendButton}
+              id="send-button"
+            >
+              Send
+            </button>
           </form>
         </div>
       </div>

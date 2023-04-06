0de61db4f4df408c93e8eabd2f1ca39969a196e6
Winn <winnieee.lam00@gmail.com>
2023-04-05 20:28:32 -0400

    problem list only lists problems

--- frontend/src/pages/Problems/Problems.js
+++ frontend/src/pages/Problems/Problems.js
@@ -1,17 +1,13 @@
 import React from "react";
 import problemStyles from "./Problems.module.css";
-import { withAuth0 } from "@auth0/auth0-react";
-import { HashLink as Link } from "react-router-hash-link";
 import * as problemService from "../../services/api/Problems.js";
 
 class Problems extends React.Component {
   constructor(props) {
     super(props);
-    const { user } = this.props.auth0;
 
     this.state = {
       problems: [],
-      emailVerified: user.email_verified,
     };
 
     problemService.getProblems().then((res) => {
@@ -20,45 +16,24 @@ class Problems extends React.Component {
   }
 
   render() {
-    const { problems, emailVerified } = this.state;
-    let problemList;
-    if (emailVerified) {
-      problemList = problems.map((problem) => (
-        <Link
-          to="/coding"
-          state={{ number: problem.number }}
-          className={`${problemStyles.problem}
+    const { problems } = this.state;
+
+    let problemList = problems.map((problem) => (
+      <div
+        className={`${problemStyles.problem}
             ${problem.difficulty === "easy" ? problemStyles.easy : ""} 
             ${problem.difficulty === "medium" ? problemStyles.medium : ""} 
             ${problem.difficulty === "hard" ? problemStyles.hard : ""}`}
-          key={problem.number}
-        >
-          <div className={problemStyles.info}>
-            <h2>
-              {problem.number}. {problem.name}
-            </h2>
-            <p>{problem.description}</p>
-          </div>
-        </Link>
-      ));
-    } else {
-      problemList = problems.map((problem) => (
-        <div
-          className={`${problemStyles.problem}
-            ${problem.difficulty === "easy" ? problemStyles.easy : ""} 
-            ${problem.difficulty === "medium" ? problemStyles.medium : ""} 
-            ${problem.difficulty === "hard" ? problemStyles.hard : ""}`}
-          key={problem.number}
-        >
-          <div className={problemStyles.info}>
-            <h2>
-              {problem.number}. {problem.name}
-            </h2>
-            <p>{problem.description}</p>
-          </div>
+        key={problem.number}
+      >
+        <div className={problemStyles.info}>
+          <h2>
+            {problem.number}. {problem.name}
+          </h2>
+          <p>{problem.description}</p>
         </div>
-      ));
-    }
+      </div>
+    ));
 
     return (
       <div className={problemStyles.problems}>
@@ -69,4 +44,4 @@ class Problems extends React.Component {
   }
 }
 
-export default withAuth0(Problems);
+export default Problems;
